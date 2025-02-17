43171abad7c79d8510c32a5568453686c5b322a1
hvillase <hernani.vr@gmail.com>
2025-02-17 13:09:35 -0600

    ejemplos js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log("aquí está la consola");
 window.alert("ahhhhh");
 document.write("Esto es texto escrito dentro del script js!!!");
+
+document.getElementById("demo").innerHTML="caddena de caracteres";
+
+let nombre = "miNombre";
+document.getElementById("nombre").innerHTML=nombre;
