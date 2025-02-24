b5e7a19c0ab4d1fed69fe713c1c6ad45d7413949
hvillase <hernani.vr@gmail.com>
2025-02-24 11:00:15 -0600

    semana3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("aquí está la consola");
 window.alert("ahhhhh");
 document.write("Esto es texto escrito dentro del script js!!!");
 
-document.getElementById("demo").innerHTML="caddena de caracteres";
+document.getElementById("demo").innerHTML="cadena de caracteres";
 
 let nombre = "miNombre";
 document.getElementById("nombre").innerHTML=nombre;
