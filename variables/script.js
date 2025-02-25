eeedf63fd6f4e487b5aadcb320022852356b09e3
hvillase <hernani.vr@gmail.com>
2025-02-25 10:04:51 -0600

    varibel

---
+++ variables/script.js
@@ -0,0 +1,9 @@
+// variables
+
+let ingrediente1 = "queso";
+let precio = 120.5;
+
+//document.write(ingrediente1);
+document.getElementById('demo').innerHTML="Un ingrediente es: " + ingrediente1;
+
+document.getElementById('demo2').innerHTML="El precio del " + ingrediente1 + " es: " + precio;
---
+++ vinculos-imagen/script.js
@@ -0,0 +1,1 @@
+console.log("Mim primer sitio web");
