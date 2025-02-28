487febef6030c4df5d35c4922e30d8b45c848f3b
hvillase <hernani.vr@gmail.com>
2025-02-28 08:52:42 -0600

    tareas 2

---
+++ tarea2/script.js
@@ -0,0 +1,8 @@
+let p1 = "Respuesta 1";
+let p2 = "Respuesta 2";
+
+document.getElementById('parr1').innerHTML=p1;
+document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);
